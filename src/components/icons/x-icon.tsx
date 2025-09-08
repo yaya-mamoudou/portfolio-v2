48a27541b73ebb145c6ce9484deb76bd0db4a46f
Yaya Mamoudou <yayamamoudou0@gmail.com>
2025-09-08 06:10:48 +0100

    build hero section

---
+++ src/components/icons/x-icon.tsx
@@ -0,0 +1,17 @@
+export default function XIcon() {
+	return (
+		<svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<g clipPath='url(#clip0_8_57)'>
+				<path
+					d='M9.47886 6.77486L15.2651 0H13.8937L8.87086 5.88229L4.85715 0H0.228577L6.29715 8.896L0.228577 16H1.60001L6.90515 9.78743L11.144 16H15.7726L9.47886 6.77486ZM7.60115 8.97371L6.98629 8.088L2.09372 1.04H4.20001L8.14743 6.728L8.76229 7.61371L13.8949 15.008H11.7886L7.60115 8.97371Z'
+					fill='#848484'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_8_57'>
+					<rect width='16' height='16' fill='white' />
+				</clipPath>
+			</defs>
+		</svg>
+	);
+}
